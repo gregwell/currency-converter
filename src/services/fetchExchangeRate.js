e62a52869c65058fa670f6cc2ecbec30023780d5
DESKTOP-27CQTIN\grzegurz <grzegorzstudzinski@hotmail.com>
2021-03-11 00:18:44 +0100

    Converted Promise chain to async/await

--- src/services/fetchExchangeRate.js
+++ src/services/fetchExchangeRate.js
@@ -1,6 +1,9 @@
-export function fetchExchangeRate() {
-    return fetch('http://api.nbp.pl/api/exchangerates/rates/a/gbp/?format=json')
-      .then(data => data.json())
-      .then(data => data.rates[0].mid)
-      .catch(err => console.log(err))
+export async function fetchExchangeRate() {
+  try {
+    const result = await fetch('http://api.nbp.pl/api/exchangerates/rates/a/gbp/?format=json');
+    const json = await result.json();
+    return json.rates[0].mid;
+  } catch (err) {
+    return console.log(err);
   }
+}
