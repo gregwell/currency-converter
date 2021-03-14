fafa288a0bf65fd05d2ee5fd25f36435eb59d1c2
DESKTOP-27CQTIN\grzegurz <grzegorzstudzinski@hotmail.com>
2021-03-14 16:13:02 +0100

    deleted unnecessary theme from Flag styles

--- src/components/Input/Flag/styles.js
+++ src/components/Input/Flag/styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-export default makeStyles((theme) => ({
+export default makeStyles({
     flag : {
         height: '17px',
         border: '1px solid #94A3BB',
@@ -8,4 +8,4 @@ export default makeStyles((theme) => ({
         padding: 0,
         margin: 0
     }
-  }));
+  });
